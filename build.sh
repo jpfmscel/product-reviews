93f3259156fc40412d05546e5c2de7da8863fca1
João Paulo Séder <jpfmscel@gmail.com>
2019-01-23 21:02:30 +0000

    Fix build order and script

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-cd product-service && ./mvnw clean package && cd review-service && ./mvnw clean package && cd auth-service && ./mvnw clean package
+docker-compose up mongo && cd product-service && ./mvnw clean package & cd review-service && ./mvnw clean package & cd auth-service && ./mvnw clean package
