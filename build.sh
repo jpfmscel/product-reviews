9a655ec80bdc09c387818415875c365038726e1f
Joao Paulo Seder <jpfmscel@gmail.com>
2019-01-24 18:09:00 +0000

    Tests on dockerfile review-service

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-docker-compose up mongo && cd product-service && ./mvnw clean package & cd review-service && ./mvnw clean package & cd auth-service && ./mvnw clean package
+docker-compose up mongo && cd product-service && ./mvnw clean package & cd review-service && ./mvnw clean package & cd auth-service && ./mvnw clean package && docker-compose down mongo
